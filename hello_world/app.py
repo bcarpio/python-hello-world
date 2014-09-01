6e049ac8c39acc1cb0a8b935f1d60bb785dda5a6
Brian Carpio <bcarpio@itriagehealth.com>
2014-09-01 13:11:07 +0000

    added a bunch of CSS and stuff

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, make_response, Response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Joe Really Is Dope"
+    hello = "Joe Really Is Dope"
+    return render_template('index.html',hello=hello)
 
 if __name__ == '__main__':
     app.debug = True
