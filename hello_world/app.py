1b76e3dc7c1dc495458af960ffd5048a8f925dfb
Brian Carpio <bcarpio@itriagehealth.com>
2014-09-01 04:34:28 +0000

    changed joe to eric

--- hello_world/app.py
+++ hello_world/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Joe Really Is Dope"
+    return "Hello World! Eric Really Is Dope"
 
 if __name__ == '__main__':
     app.debug = True
