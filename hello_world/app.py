aebefc885eeda01b7730acd8998d074be8eb80af
Brian Carpio <bcarpio@itriagehealth.com>
2014-09-01 13:50:09 +0000

    updated text to make outside call

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect, make_response, Response, jsonify
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     hello = {'Who' : 'Joe', 'What': 'The Shit'}
-    return jsonify(hello)
+    r = requests.get("http://ip.jsontest.com/")
+    dict = {'data': r.json(), 'text': hello}
+    return jsonify(dict)
 
 if __name__ == '__main__':
     app.debug = True
